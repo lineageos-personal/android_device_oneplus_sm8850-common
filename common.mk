a606459ea40e5d430d7279099c95fad40573d0ef
blazerpaul15 <blazerpaul15@gmail.com>
2026-07-09 20:32:24 +0900

    sm8850-common: Include oplus camera

--- common.mk
+++ common.mk
@@ -272,6 +272,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 endif
 
+# OPlus Camera
+$(call inherit-product-if-exists, device/oneplus/macan-camera/opluscamera.mk)
+
 # OPlus dummy services
 PRODUCT_PACKAGES += \
     vendor.oplus.hardware.commondcs-service \
